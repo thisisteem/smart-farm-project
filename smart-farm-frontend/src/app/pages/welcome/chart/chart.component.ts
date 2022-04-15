675045008ce42ad10f53a5bb929b6f2fc35e756c
Homeward-creator <58843033+Homeward-creator@users.noreply.github.com>
2022-04-15 23:51:39 +0700

    add graph

--- smart-farm-frontend/src/app/pages/welcome/chart/chart.component.ts
+++ smart-farm-frontend/src/app/pages/welcome/chart/chart.component.ts
@@ -1,6 +1,44 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { format, compareAsc } from 'date-fns'
+import 'chartjs-adapter-date-fns';
+
+
+const dates = [
+  // new Date('2022-01-01T07:01:36.386Z'),
+  // new Date('2022-01-02T02:01:36.386Z'),
+  // new Date('2022-01-03T04:01:36.386Z'),
+  // new Date('2022-01-04T12:01:36.386Z'),
+  // new Date('2022-01-05T16:01:36.386Z'),
+  // new Date('2022-01-06T12:01:36.386Z'),
+  // new Date('2022-01-07T12:01:36.386Z'),
+  '2022-01-01T07:01:36.386Z',
+  '2022-01-02T02:01:36.386Z',
+  '2022-01-03T04:01:36.386Z',
+  '2022-01-04T12:01:36.386Z',
+  '2022-01-05T16:01:36.386Z',
+  '2022-01-06T12:01:36.386Z',
+  '2022-01-07T12:01:36.386Z',
+  // new Date(2022, 1, 1),
+  // new Date(2022, 1, 2),
+  // new Date(2022, 1, 3),
+  // new Date(2022, 1, 4),
+  // new Date(2022, 1, 5),
+  // new Date(2022, 1, 6),
+  // new Date(2022, 1, 7),
+]
+var data_temp = [25.0, 26.0, 28, 24, 23, 25, 26]; // wait for dataset
+var humidity = [28, 48, 40, 19, 86, 27, 63]; // wait for dataset
+var dirt_humid = [48, 39, 40, 100, 15, 26, 87]; // wait for dataset
+var i:number; 
+var array_labels: Date[] = [];
+
+for (i = 0; i<dates.length;i++){
+  var x = dates[i];
+  
+  array_labels.push(new Date(x));
+}
 
 @Component({
   selector: 'app-chart',
@@ -11,19 +49,19 @@ export class ChartComponent implements OnInit {
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
+        data: data_temp,
+        label: 'อุณหภูมิ (°C)',
+        backgroundColor: 'rgba(0,0,250,0.2)',
+        borderColor: 'rgba(0,0,250,1)',
+        pointBackgroundColor: 'rgba(0,0,250,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        pointHoverBorderColor: 'rgba(0,0,250,0.8)',
         fill: 'origin',
       },
       {
-        data: [28, 48, 40, 19, 86, 27, 90],
-        label: 'Series B',
+        data: humidity,
+        label: 'ความชื้น (%)',
         backgroundColor: 'rgba(77,83,96,0.2)',
         borderColor: 'rgba(77,83,96,1)',
         pointBackgroundColor: 'rgba(77,83,96,1)',
@@ -33,9 +71,8 @@ export class ChartComponent implements OnInit {
         fill: 'origin',
       },
       {
-        data: [180, 480, 770, 90, 1000, 270, 400],
-        label: 'Series C',
-        yAxisID: 'y-axis-1',
+        data: dirt_humid,
+        label: 'ความชื้นดิน (%)',
         backgroundColor: 'rgba(255,0,0,0.3)',
         borderColor: 'red',
         pointBackgroundColor: 'rgba(148,159,177,1)',
@@ -45,31 +82,38 @@ export class ChartComponent implements OnInit {
         fill: 'origin',
       },
     ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: array_labels,
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
     responsive: true,
+    locale: 'th-TH',
     elements: {
       line: {
-        tension: 0.5,
+        tension: 0.3,
       },
     },
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
-      x: {},
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+          
+        }
+      },
       'y-axis-0': {
         position: 'left',
       },
-      'y-axis-1': {
-        position: 'right',
-        grid: {
-          color: 'rgba(255,0,0,0.3)',
-        },
-        ticks: {
-          color: 'red',
-        },
-      },
+      // 'y-axis-1': {
+      //   position: 'right',
+      //   grid: {
+      //     color: 'rgba(255,0,0,0.3)',
+      //   },
+      //   ticks: {
+      //     color: 'red',
+      //   },
+      // },
     },
 
     plugins: {
